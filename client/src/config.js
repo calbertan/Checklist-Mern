038983c54ad5a88576c1fc53e9ff915369927269
Christoper Tan <christopertan@Supports-MacBook-Pro.local>
2023-06-13 16:59:54 -0700

    hardcoded api_url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: process.env.REACT_APP_API_URL || ''
+    API_URL: process.env.REACT_APP_API_URL || 'http://mern-api-fe805a-prod.apps.clab.devops.gov.bc.ca'
   };
   
 export default config;
